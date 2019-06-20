59751f516f7e22ce53fcddd1cbe98ed3a37323ca
Vajira96 <vajirabandara123@gmail.com>
2019-06-20 22:43:48 +0530

    first commit

---
+++ app/src/main/java/lk/ac/kln/pizza/Log_in_form.java
@@ -0,0 +1,13 @@
+package lk.ac.kln.pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Log_in_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_form);
+    }
+}
---
+++ app/src/main/java/lk/ac/kln/pizza/signUp_form.java
@@ -0,0 +1,14 @@
+package lk.ac.kln.pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signUp_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_form);
+        getSupportActionBar().setTitle("Sign Up Form");
+    }
+}
